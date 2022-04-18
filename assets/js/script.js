cd02af17577d604274d8eb81b6064b496c4368be
ilromali28 <il.roma.li.28@gmail.com>
2022-04-18 15:08:18 +0300

    "benefits" swipe feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,15 @@ $(document).ready(function() {
         }
         $('.dd_opened').removeClass('dd_opened');
     });
+
+    $(".switcher-contents__item").swipe({
+        swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+            var currentCheckbox = $(this).find('input:checked');
+            if (direction == "right") {
+                currentCheckbox.prev().prop("checked", true);
+            } else if (direction == "left") {
+                currentCheckbox.next().prop("checked", true);
+            }
+        }
+    });
 });
